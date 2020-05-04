5d178850a9ee8d59afbbbb2808337a2f072e8c09
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-04 21:49:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
-
-def countdown
+def countdown(num)
+  while num > 0 
+    puts "#{num} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  while num > 0 
+    puts "#{num} SECOND(S)!"
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
+
